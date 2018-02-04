3df2a7addce9240c56c60597228ad8962dd40681
Piotr Stęclik <piotr@steclik.com>
2018-02-04 11:25:35 +0100

    another for loop translation test

--- Lang.Python.Tests/LoopsTest.cs
+++ Lang.Python.Tests/LoopsTest.cs
@@ -42,5 +42,43 @@ class DemoClass:
             };
             CheckTranslation(cs, info);
         }
+        
+        [Fact]
+        public void T02_Should_convert_for_loop()
+        {
+            var cs       = @"
+using System;
+using Lang.Python;
+
+namespace Demo01
+{
+    [IgnoreNamespaceAttribute]
+    public class DemoClass
+    {
+        public static void Test()
+        {
+            for(int i=3; i< 10; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
+";
+            var expected = @"
+class DemoClass:
+    @staticmethod
+    def Test(cls):
+        for i in range(3, 10):
+            print(i)    
+";
+            var info     = new Info
+            {
+                Compare = expected,
+                Ref     = new[]
+                {
+                    typeof(Enumerable).Assembly
+                }
+            };
+            CheckTranslation(cs, info);
+        }
     }
 }
